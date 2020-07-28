8770f012d264040689b6d51418db01cd24efdfdb
Linh Nguyen <linh.ng754@gmail.com>
2020-07-27 20:38:38 -0400

    architecture exploration

---
+++ pages/about.js
@@ -0,0 +1,37 @@
+import Head from "next/head";
+import Link from "next/link";
+import styles from "../styles/Home.module.css";
+
+const Home = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>ABOUT ME</h1>
+
+        <h3>
+          <Link href="/">
+            <a>Back home</a>
+          </Link>
+        </h3>
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{" "}
+          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+        </a>
+      </footer>
+    </div>
+  );
+};
+
+export default Home;
